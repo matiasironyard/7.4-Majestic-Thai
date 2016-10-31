05e6d652552b051330e31c8f2dbec742bd54c654
Matias Mariani <matiasironyard@gmail.com>
2016-10-30 20:37:25 -0400

    functinal

--- app/scripts/components/order.jsx
+++ app/scripts/components/order.jsx
@@ -7,34 +7,37 @@ var CheckoutComponent = React.createClass({
   mixins: [Backbone.React.Component.mixin],
   handleSubmit: function(e){
     e.preventDefault();
+    this.props.currentOrders.each(function(order){
+      order.save()
+    });
   },
 render: function(){
   var OrderedDishes = this.props.currentOrders.map(function(item){
     var dish = item.get('dish');
-    var price = parseInt(item.get('price'));
+    var price = item.get('price');
     return(
       <div key={item.cid}>
         <span>{dish}</span><span>{price}</span>
       </div>
     )
-  }).reduce(function(previous, currentDish){
-    return previous +=currentDish.get('price');
-  },0);
+  });
   // var prices = OrderedDishes.map(function(item){
   //   return item.get('price');
   // });
   // var convertedPrices = prices.map(Number);
   // console.warn(convertedPrices);
-  // var total = this.props.currentOrders.reduce(function(previous, currentDish){
-  //   return previous +=currentDish.get('price');
-  // },0)
+  var total = this.props.currentOrders.reduce(function(previous, currentDish){
+    previous = Number(previous)
+    currentDish = Number(currentDish.get('price'))
+    return previous + currentDish;
+  },0)
   return(
-    <div className="col-md-4 order-col">
+    <div className="col-md-12 order-col">
       <h4 className="menu">Order</h4>
       <ul className="order-list-view">
         {OrderedDishes}
       </ul>
-        <span>Total:</span><button type="submit" onClick={this.handleSubmit}>Place Your Order</button>
+        <span>Total: {total.toFixed(2)}</span><button type="submit" onClick={this.handleSubmit}>Place Your Order</button>
   </div>
   );
 }
